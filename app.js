389cb77070c39e06663c477c21ac92081fd94ed5
PavanMallela <mallelapavank@gmail.com>
2017-07-13 23:25:10 +0530

    initial commit

---
+++ app.js
@@ -0,0 +1,21 @@
+const http = require('http'); //http module
+const fs = require('fs'); //file system module
+
+const hostname = '127.0.0.1';
+const port = 3000;
+
+fs.readFile('index.html', (err, html) => {
+  if(err){
+    throw err;
+  }
+  const server = http.createServer((request, response) => {
+    response.statusCode = 200;
+    response.setHeader('Content-type', 'text/html');
+    response.write(html)
+    response.end();
+  });
+
+  server.listen(port, hostname, () => {
+    console.log("Server started on port " + port);
+  });
+});
